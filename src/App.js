0c956be26df141d0ed9d55a98640e97253edc2f9
Raghav <rsharma@duck.com>
2023-07-25 15:58:17 -0500

    Project Start

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <h2>React on localhost!</h2>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <h1>Hello React!!!!</h1>;
+}
+
+// How to render in React v18
+// if you go to the folder 'public' and then open
+// index.html, there will be <div> in there with the
+// the id of "root". We need to put that id in here
+// so App() is rendered to display on webpage
+
+// Strict mode during development it will render
+// components twice and check for bugs
+// react will also check for outdated parts of
+// React API
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// if you want to start application, use the command:
+//      npm start
+// to find the file with all the npm commands, locate
+// the package.json file in the project
+// it will be under scripts
